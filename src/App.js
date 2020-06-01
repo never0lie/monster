0b69b7358776aa6bd560247aa3a1802ddef4aca3
James Xu <sizhan_x@utexas.edu>
2020-06-01 01:00:56 -0500

    did some revision to App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component{
     .then(response => response.json())
     .then(users => this.setState({monsters : users}));
   }
+
+  handleChange = (e) => {
+    this.setState( { searchField: e.target.value } )
+  }
   
   render() {
     const { monsters, searchField } = this.state;
@@ -31,7 +35,7 @@ class App extends Component{
       <h1>Monsters Rolodex</h1>
       <SearchBox 
         placeholder = 'search monsters'
-        handleChange = {e => this.setState({searchField: e.target.value})}
+        handleChange = {this.handleChange}
       />
       <CardList monsters = {filteredMonsters} />
       </div>
